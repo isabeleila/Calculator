db4cc1637445f5a3594bb2d6eafc868cb7f2f102
isabeleila <leilaiayers@gmail.com>
2025-02-11 10:34:40 -0500

    User is able to input floating numbers

--- Calculator.java
+++ Calculator.java
@@ -4,26 +4,26 @@ public class Calculator{
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("Enter your first whole number: ");
-        int num1 = scan.nextInt();
+        System.out.println("Enter your first number: ");
+        float num1 = scan.nextFloat();
 
-        System.out.println("Enter your second whole number: ");
-        int num2 = scan.nextInt();
+        System.out.println("Enter your second number: ");
+        float num2 = scan.nextFloat();
 
         System.out.println("Would you like to add, subtract, multiply or divide? Enter in add, subtract, multiply, divide.");
         String answer = scan.next();
 
         if(answer.equals("add")){
-            int sum = num1 + num2;
+            float sum = num1 + num2;
             System.out.println("The sum of: " + num1 + " + " + num2 + " = " + sum);
         } else if(answer.equals("subtract")){
-            int diff = num1 - num2;
+            float diff = num1 - num2;
             System.out.println("The difference of: " + num1 + " - " + num2 + " = " + diff);
         } else if(answer.equals("multiply")){
-            int product = num1 * num2;
+            float product = num1 * num2;
             System.out.println("The product of: " + num1 + " * " + num2 + " = " + product);
         }else if(answer.equals("divide")){
-            int quotient = num1/num2;
+            float quotient = num1/num2;
             System.out.println("The quotient for: " + num1 + "/" + num2 + " = " + quotient);
         }else{
             System.out.println("Please enter a valid response.");
