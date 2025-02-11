f36ca23600c931e81d94c38be378b4e9f6c3b019
isabeleila <leilaiayers@gmail.com>
2025-02-11 10:01:36 -0500

    Adding Using Whole Numbers

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Calculator{
     public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
         
+        System.out.println("Enter your first whole number: ");
+        int num1 = scan.nextInt();
+
+        System.out.println("Enter your second whole number: ");
+        int num2 = scan.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum of: " + num1 + " + " + num2 + " = " + sum);
+
+        scan.close();
     }
 }
